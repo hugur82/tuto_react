81d52d5e7f87297ae29d0663a43cc45ec63b3c2d
Hakan <ugur_hakan@yahoo.fr>
2023-08-07 01:00:52 +0200

    fin projet 5 et debut 6

---
+++ 01-starting-project 6/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Layout/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
+export default App;
