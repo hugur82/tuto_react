4407ff3f7797e3c67ed1eee1525b3041b3795bd7
Hakan <ugur_hakan@yahoo.fr>
2024-07-04 22:34:57 +0200

    update set interval with clean the useEffect, and add a key to QuestionTimer component

--- 12 Building a quiz app/src/App.jsx
+++ 12 Building a quiz app/src/App.jsx
@@ -1,3 +1,16 @@
-function App() {}
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
+}
 
 export default App;
---
+++ 12 Building a quiz app/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logoImg from "../assets/quiz-logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logoImg} alt="" />
+      <h1>React quiz</h1>
+    </header>
+  );
+};
+
+export default Header;
