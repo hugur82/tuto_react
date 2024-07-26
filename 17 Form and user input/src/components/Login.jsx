53d5f007302d4b1750d9fe6fea0a33627830ced8
khuq <ugur_hakan@yahoo.fr>
2024-07-26 11:24:35 +0200

    finish validation logic, vid 260

--- 17 Form and user input/src/components/Login.jsx
+++ 17 Form and user input/src/components/Login.jsx
@@ -1,17 +1,23 @@
 import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(
-      "email = ",
-      email.current.value,
-      " password =",
-      password.current.value
-    );
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+    setEmailIsInvalid(false);
   }
 
   return (
@@ -22,6 +28,9 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && "Please enter a valid email"}
+          </div>
         </div>
 
         <div className="control no-margin">
