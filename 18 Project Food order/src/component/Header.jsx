845afce65a9f9f97c0b2b18a51821abba2a0251a
khuq <ugur_hakan@yahoo.fr>
2024-07-27 22:22:11 +0200

    Project food - started

---
+++ 18 Project Food order/src/component/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div id="main-header">
+        <div id="title"></div>
+        <button></button>
+      </div>
+    </>
+  );
+};
+
+export default Header;
