60b2e6c0449ecc021a8762d3a3a8868f08d47551
khuq <ugur_hakan@yahoo.fr>
2024-07-28 23:28:04 +0200

    handle the moodal, the cart

--- 18 Project Food order/src/App.jsx
+++ 18 Project Food order/src/App.jsx
@@ -1,14 +1,20 @@
-import { useEffect, useState } from "react";
+import Cart from "./components/Cart";
 import Header from "./components/Header";
 import Meals from "./components/Meals";
+import Checkout from "./components/UI/Checkout";
+import { CartContextProvider } from "./store/CartContext";
+import { UserProgressContextProvider } from "./store/UserProgressContext";
 
 function App() {
   return (
-    <>
-      <Header />
-
-      <Meals />
-    </>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+        <Checkout />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
--- 18 Project Food order/src/components/Meals.jsx
+++ 18 Project Food order/src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import MealItem from "./MealItem";
 
 const Meals = () => {
   const [loadedMeals, setLoadedMeals] = useState([]);
@@ -21,7 +22,7 @@ const Meals = () => {
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => (
-        <li key={meal.id}>{meal.name}</li>
+        <MealItem meal={meal} key={meal.id} />
       ))}
     </ul>
   );
