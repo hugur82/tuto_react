03cf78d219c7710b8da52c1d95d730fc0f106bff
khuq <ugur_hakan@yahoo.fr>
2024-07-28 23:31:24 +0200

    update ep 277

---
+++ 18 Project Food order/src/components/CartItem.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { currencyFormatter } from "../util/formatting";
+import CartContext from "../store/CartContext";
+
+const CartItem = ({ name, quantity, price, onIncrease, onDecrease }) => {
+  return (
+    <li className="cart-item">
+      <p>
+        {name} - {quantity} x {currencyFormatter.format(price)}
+      </p>
+      <p className="cart-item-actions">
+        <button onClick={onDecrease}>-</button>
+        <span>{quantity}</span>
+        <button onClick={onIncrease}>+</button>
+      </p>
+    </li>
+  );
+};
+
+export default CartItem;
---
+++ 18 Project Food order/src/util/formatting.js
@@ -0,0 +1,4 @@
+export const currencyFormatter = new Intl.NumberFormat("fr-FR", {
+  style: "currency",
+  currency: "EUR",
+});
