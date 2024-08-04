555555b55dbe04933a2d84eb67644e3472006621
khuq <ugur_hakan@yahoo.fr>
2024-08-05 01:54:55 +0200

    Chapter 21 , react router, finished the video 334

---
+++ 21 multPage SPA with react router/src/pages/Home.js
@@ -0,0 +1,20 @@
+import { Link, useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("./products");
+  };
+
+  return (
+    <>
+      <h1>Home</h1>
+      <p>
+        We want to <Link to="/products">go to our products list</Link>
+      </p>
+      <button onClick={handleClick}>produit</button>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ 21 multPage SPA with react router/src/pages/Products.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+  { id: 1, title: "Laptop" },
+  { id: 2, title: "Smartphone" },
+  { id: 3, title: "Headphones" },
+];
+
+const ProductsPage = () => {
+  return (
+    <>
+      <h1>ProductsPage</h1>
+      <ul>
+        {PRODUCTS.map((prod) => (
+          <li key={prod.id}>
+            <Link to={`/products/${prod.id}`}>{prod.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ProductsPage;
