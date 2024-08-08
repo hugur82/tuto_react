b65b33592200b99b6d820c51644a78d4bbf7aecf
Hakan <ugur_hakan@yahoo.fr>
2024-08-08 18:54:10 +0200

    finish the defer leson, video 361

--- 21.2 Onwards to a new project setup/frontend/src/App.js
+++ 21.2 Onwards to a new project setup/frontend/src/App.js
@@ -1,53 +1,70 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/Home";
-import RootLayout from "./pages/Root";
-import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import EditEventPage from "./pages/EditEvent";
+import ErrorPage from "./pages/Error";
 import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
 } from "./pages/EventDetail";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
-import EditEventPage from "./pages/EditEvent";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
-import ErrorPage from "./pages/Error";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
+import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "events",
+        element: <EventsRootLayout />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          {
+            path: ":eventId",
+            id: "event-detail",
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction,
+              },
+              {
+                path: "edit",
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
+            ],
+          },
+          {
+            path: "new",
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
+        ],
+      },
+      {
+        path: "newsletter",
+        element: <NewsletterPage />,
+        action: newsletterAction,
+      },
+    ],
+  },
+]);
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout />,
-      errorElement: <ErrorPage />,
-      children: [
-        { index: true, element: <HomePage /> },
-        {
-          path: "events",
-          element: <EventsRootLayout />,
-          children: [
-            {
-              index: true,
-              element: <EventsPage />,
-              loader: eventsLoader,
-            },
-            {
-              path: ":eventId",
-              id: "event-detail",
-              loader: eventDetailLoader,
-              children: [
-                {
-                  index: true,
-                  element: <EventDetailPage />,
-                  action: deleteEventAction,
-                },
-                { path: "edit", element: <EditEventPage /> },
-              ],
-            },
-            { path: "new", element: <NewEventPage />, action: newEventAction },
-          ],
-        },
-      ],
-    },
-  ]);
-  return <RouterProvider router={router}></RouterProvider>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- 21.2 Onwards to a new project setup/frontend/src/pages/EditEvent.js
+++ 21.2 Onwards to a new project setup/frontend/src/pages/EditEvent.js
@@ -7,7 +7,7 @@ const EditEventPage = () => {
 
   return (
     <>
-      <EventForm event={data.event} />
+      <EventForm method="patch" event={data.event} />
     </>
   );
 };
